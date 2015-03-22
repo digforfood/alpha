cd1579481e6f1946473b9e9e573fa3318d8f2f04
digforfood <pgeekadmin@gmail.com>
2015-03-22 16:20:34 +0300

    add setMap

--- ae.js
+++ ae.js
@@ -1,5 +1,7 @@
 var AE = function(settings){
 	var that = this;
+	this.JSONDATA = 'jsondata';
+	this.JSONURL = 'jsonurl';
 	this.settings = settings||{};
 
 	this.imgDir = (this.settings.imgDir) ? this.settings.imgDir+'/' : '';
@@ -7,6 +9,7 @@ var AE = function(settings){
 	this.baseRate = this.settings.baseRate || 30;
 	this.windowWidth = this.settings.windowWidth || 640;
 	this.windowHeight = this.settings.windowHeight || 480;
+	this.tilemap = false;
 
 	this.startButton = $('<div id="startButton" style="width:'+this.windowWidth+'px;height:'+this.windowHeight+'px;background:'+this.startImg+' #A8E1FF;">');
 	this.gameFrame = $('<div id="gameFrame" style="display:none;width:'+this.windowWidth+'px;height:'+this.windowHeight+'px;">');
@@ -15,9 +18,24 @@ var AE = function(settings){
 		$('body').append('<div id="game" style="position:relative;width:'+this.windowWidth+'px;height:'+this.windowHeight+'px;overflow:hidden;">');
 		$('#game').append(this.startButton);
 
-		this.startButton.click(function() {
+		this.startButton.click(function(){
 			that.startGame();
 		});
+	};
+
+	this.setTilemap = function(data,type){
+		if(type == this.JSONDATA){
+			this.tilemap = data;
+		}else if(type == this.JSONURL){
+			$.ajax({
+				url: data,
+				async: false,
+				dataType: 'json',
+				success: function(json){
+					this.tilemap = json;
+				}
+			});
+		}		
 	};
 
 	this.startGame = function(){
