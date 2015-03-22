3e65f251ce27ee48584482c97a2f0739bec3b7cd
digforfood <pgeekadmin@gmail.com>
2015-03-22 22:19:15 +0300

    tilemap create

--- ae.js
+++ ae.js
@@ -11,16 +11,27 @@ var AE = function(settings){
 	this.windowHeight = this.settings.windowHeight || 480;
 	this.tilemap = false;
 
+	this.gameObjectsArr = [];
+
 	this.startButton = $('<div id="startButton" style="width:'+this.windowWidth+'px;height:'+this.windowHeight+'px;background:'+this.startImg+' #A8E1FF;">');
 	this.gameFrame = $('<div id="gameFrame" style="display:none;width:'+this.windowWidth+'px;height:'+this.windowHeight+'px;">');
+	this.sprite = $('<div class="sprite" style="position:absolute;overflow:hidden;"></div>');
 
 	this.init = function(){
+
+		this.createTilemap();
+
 		$('body').append('<div id="game" style="position:relative;width:'+this.windowWidth+'px;height:'+this.windowHeight+'px;overflow:hidden;">');
 		$('#game').append(this.startButton);
 
 		this.startButton.click(function(){
 			that.startGame();
 		});
+	};
+
+	this.Tiles = function(sprite,animation){
+		this.sprite = sprite;
+		this.animation = animation;
 	};
 
 	this.setTilemap = function(data,type){
@@ -36,6 +47,44 @@ var AE = function(settings){
 				}
 			});
 		}		
+	};
+
+	this.createTilemap = function(){
+		if(this.tilemap){			
+			var x = 0;
+			var y = 0;
+			var tileWidth = this.tilemap.tiles.width;
+			var tileHeight = this.tilemap.tiles.height;
+			var tilemapWidth = this.tilemap.data[0].length;
+			var tilemapHeight = this.tilemap.data.length;
+			var tilemapFrame = $('<div class="tilemapFrame" style="position:absolute"></div>');
+
+			for(var i=0; i < tilemapHeight; i++){
+				for(var j=0; j < tilemapWidth; j++){
+					if(this.tilemap.data[i][j] > 0){
+						var options = {
+							x: x + j*tileWidth,
+							y: y + i*tileHeight,
+							width: tileWidth,
+							height: tileHeight
+						};
+
+						var sprite = this.sprite.clone().css({
+							left: options.x,
+							top: options.y,
+							width: options.width,
+							height: options.height}
+						).addClass("row_"+i).addClass("cell_"+j).data("AE", options);
+						
+						var animation = {};
+						var tile = new this.Tiles(sprite,animation);
+						tilemapFrame.append(tile.sprite)
+						this.gameObjectsArr.push(tile);
+					}
+				}
+			}
+			this.gameFrame.append(tilemapFrame)
+		}
 	};
 
 	this.startGame = function(){
